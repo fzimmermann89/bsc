1f85a0b236614f22b5be467c0b4b344c1de0528e
Felix Zimmermann <fzimmermann89@gmail.com>
2016-03-02 22:21:49 +0100

    play around
    Former-commit-id: 8ad1ddd5a95f2f5736c4e2a53bfc4563c6f3af20

--- recon/test_recon.m
+++ recon/test_recon.m
@@ -1,53 +1,132 @@
-hioiter=50;
-iter=400;
-rec=10;
-%load image and create fourier amplitude
-input=gpuArray(rgb2gray(imread('input_2.png')));
-tic
-plan(1).algo='hio';
-plan(1).iter=5;
-plan(2).algo='er';
-plan(2).iter=500;
-% plan(3).algo='hio';
-% plan(3).iter=5;
-% plan(4).algo='er';
-% plan(4).iter=50;
+refRadius=25;
+refError=1.1;
+maskScale=0.03%0.03%.1;
+sigma=10;
+useHoloSupport=false;
+%create reconstruction plan
+plan=reconPlan();
+% plan.addStep('hio',500);
+% plan.addStep('er',500);
+% for n=0:10
+% plan.addStep('hio',200);
+%   plan.addStep('er',1);
+% end
+% plan.addStep('errp',100);
+%
+for n=0:15
+    plan.addStep('hio',200);
+    plan.addStep('er',1);
+    plan.addStep('sw',1);
+    plan.addStep('show')
+end
+for n=0:15
+    plan.addStep('hio',200);
+    plan.addStep('er',1);
+    plan.addStep('hio',200);
+    plan.addStep('sw',1);
+    plan.addStep('show')
+end
+for n=0:20
+    plan.addStep('hio',200);
+    plan.addStep('er',1);
+%     plan.addStep('show')
+end
+plan.addStep('er',100);
 
-input=(double(padarray(input,size(input)/2,0))/255);
+
+
+
+%load image
+input=double(gpuArray(rgb2gray(imread('input_2.png'))))/255;
+
+% %create Reference
+% [xx,yy]=meshgrid(-refRadius:1:refRadius);
+% refImage=xx.^2+yy.^2<refRadius^2;
+% input(end-2*refRadius:end,end-2*refRadius:end)=refImage;
+
+%pad
 N=size(input);
+input=padarray(input,N,0);
+Npadded=size(input);
 
 
+
+%create Mask
+[mask,softmask,outermask]=circularMask(Npadded,maskScale,sigma);
+
+%create scatterImage
 Finput=ft2(input);
 absFinput=abs(Finput).^2;
+scatterImage=absFinput.*mask;
 
 %create support
-support=holoSupport(absFinput);
+[start,support,cross]=holoSupport(scatterImage.*softmask,refRadius*refError);
+if ~useHoloSupport
+    support=abs(ift2(scatterImage.*softmask))>0.05;
+    support = imfill(support, 'holes');
+    support = imdilate(support,ones(3));
+    
+    % support=padarray(support,N,0);
+    start=support.*rand(size(scatterImage));
+end
+
+%do deconvolution
+filteredRef=maskfilter(refImage,softmask,size(cross));
+deconv=deconvwnr(gather(cross),filteredRef,10);
+filteredDeconv=maskfilter(deconv,softmask,size(deconv));
 
 %initialize
-amplitude=sqrt(absFinput);
-randPhase=rand(size(absFinput));
-curImage=support.*rand(size(absFinput)).*exp(2i*pi*randPhase);
-errors=[];
+amplitude=sqrt(scatterImage);
 
+randPhase=rand(size(scatterImage));
+curImage=start.*exp(2i*pi*randPhase);
+errors=zeros(1,plan.iterations);
+
+nerror=1;
+tic
 for nplan=1:length(plan)
-    switch plan(nplan).algo
+    
+    step=plan.getStep(nplan);
+    switch step.method
         case 'hio'
-            for niter=1:plan(nplan).iter
-                [curImage,err]=HIOiter(amplitude, curImage, support );
-                errors(end+1)=err;
-                %                 fprintf('%f\n',err);
+            for niter=1:step.iterations
+                [curImage,err]=HIOiter(amplitude, curImage, support, mask );
+                errors(nerror)=err;
+                nerror=nerror+1;
+                
             end
         case 'er'
-            for niter=1:plan(nplan).iter
-                [curImage,err]=ERiter(amplitude, curImage, support );
-                %                 fprintf('%f\n',err);
-                errors(end+1)=err;
+            for niter=1:step.iterations
+                [curImage,err]=ERiter(amplitude, curImage, support, mask);
+                errors(nerror)=err;
+                nerror=nerror+1;
+                
             end
+        case 'errp'
+            %CAVE: support needs to be placed the same way as image
+            for niter=1:step.iterations
+                [curImage,err]=ERiterRealPos(amplitude, curImage, support, mask);
+                errors(nerror)=err;
+                nerror=nerror+1;
+            end
+        case 'sw'
+            sigma=10;
+            relThreshold=1;
+            support=SW(curImage,relThreshold,sigma);
+            errors(nerror)=errors(nerror-1);
+            nerror=nerror+1;
+        case 'show'
+            fprintf('%.2f%% done, error:%.2f\n',nplan/length(plan)*100,errors(nerror-1));
+            figure(5);imagesc(support); title('support');
+            figure(6);imagesc(abs(curImage));title('cur. Image')
+            figure(2); plot(errors); title('error');
         otherwise
             warning('unknown plan');
     end
+    
+    drawnow;
 end
-fprintf('final: %f\n',errors(end));
+toc
 %     % curImage=support;
 %     %run ER
 %     errors=zeros(iter/rec);
@@ -74,12 +153,12 @@ fprintf('final: %f\n',errors(end));
 
 
 figure(1)
-subplot(2,2,1);imagesc(input);axis square;colormap gray;
-subplot(2,2,2);imagesc(abs(ift2(absFinput)));axis square;colormap gray;caxis([0,24])
-subplot(2,2,3);imagesc(support);axis square;colormap gray;
-subplot(2,2,4);imagesc(abs(curImage));axis square;colormap gray;
+subplot(3,2,1);imagesc(log(scatterImage));axis square;colormap(flipud(gray));title('scatterImage');
+subplot(3,2,2);imagesc(abs(ift2(scatterImage)));axis square;colormap(flipud(gray));caxis([0,100]),title('holo');
+subplot(3,2,3);imagesc(support);axis square;colormap(flipud(gray));title('support');
+subplot(3,2,4);imagesc(start);axis square;colormap(flipud(gray));title('start');
+subplot(3,2,5);imagesc(abs(curImage));axis square;colormap(flipud(gray));title('final');
+subplot(3,2,6);imagesc(abs(deconv));axis square;colormap(flipud(gray));title('deconv');
 
 figure(2)
-plot(errors);
-
-toc
+semilogy(errors);
