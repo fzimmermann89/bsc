afb60fb67608c43fdfbce58b5d34638d5f1e3453
Felix Zimmermann <fzimmermann89@gmail.com>
2016-03-24 20:08:32 +0100

    add margin to evanescene mask

--- simulation/thibault.m
+++ simulation/thibault.m
@@ -2,13 +2,13 @@
 function exitWave=thibault(wavelength,objects,N,dx,gpu)
     % calculate exitWave after scene.
     % wavelength (in nm),objects (cell arra),N,dx,distanceDetektor,gpu (bool use gpu),debug (bool show progress)
+    
     debug=false;    ndebug=0;
     deltaz=wavelength;
     k=2*pi/wavelength;
-
+    
     Lz=dx*N/2; %max z values are half of N because Nx,Ny must be padded
-
-
+      
     if gpu
         %input wave
         waveF=ft2(gpuArray.ones(N,N))/(dx^2);
@@ -17,17 +17,17 @@ function exitWave=thibault(wavelength,objects,N,dx,gpu)
         waveF=ft2(ones(N,N))/(dx^2);
         zero=@()zeros(N,N);
     end
-
+    
     %prepare Objects and retrieve slice functions
     for nobj=length(objects):-1:1
         slicefun{nobj}=objects{nobj}.prepareSliceMethod(N,dx,gpu);
     end
-
+    
     %Propagator in fourier space for single step
     [propagator,factor]=getPropagatorAndFactor(N,dx,wavelength,deltaz);
-
+    
     for z=-Lz/2:deltaz:Lz/2
-
+        
         %output of progress if enabled every 100 iterations
         if debug&&ndebug==100
             waveR=gather(ift2(waveF)*dx^2);
@@ -39,14 +39,14 @@ function exitWave=thibault(wavelength,objects,N,dx,gpu)
         else
             ndebug=ndebug+1;
         end
-
+        
         %get slices deltan
         dnSlice=zero();
         for nobj=1:length(objects)
             bd=-objects{nobj}.delta+1i*objects{nobj}.beta;
             dnSlice=dnSlice+slicefun{nobj}(z)*bd;
         end
-
+        
         %multislice, vgl thibault
         if any(dnSlice(:))
             delta=dnSlice.*(ift2(waveF)*(dx^2));
@@ -55,37 +55,39 @@ function exitWave=thibault(wavelength,objects,N,dx,gpu)
             waveF=(waveF.*propagator);
         end
     end
-
+    
     exitWave=ift2(waveF)*dx^2;
-
+    
     %check intensity, should be close to 1 without absorption
     fprintf('Check= %f (should be ~1 w/o absorption)\n',  sum(abs(exitWave(:)))/(N*N));
-
+    if any(isnan(exitWave))
+        error 'NaN occured'
+    end
+    
     %unlock objects
     for nobj=length(objects):-1:1
         objects{nobj}.unlock();
     end
-
+    
     function [propagator,factor]=getPropagatorAndFactor(N,dx,wavelength,deltaz)
         %propagator and factor as in thibault
         %seperate function to create namespace for tmp variables
         k=2*pi/wavelength;
+        
         if gpu
             range=gpuArray.linspace(-N/2,N/2-1,N)*(2*pi/(dx*N));
         else
             range=linspace(-N/2,N/2-1,N)*(2*pi/(dx*N));
         end
         [qx,qy]=meshgrid(range);
-
-        evanescence_mask=k^2>(qx.^2+qy.^2);
-        kdiff=sqrt(complex(k^2-(qx.^2+qy.^2)));
-        %         kdiff=kdiff.*evanescence_mask;
-        %%TODO: +/i, wo i's.. irgendwas komisch
-        %oder in multislice
+        
+        evanescence_mask=k^2*0.8>(qx.^2+qy.^2);         %0.8 is safety margin
+        kdiff=sqrt(complex(k^2-(qx.^2+qy.^2)))-k;
         propagator=exp(1i*deltaz*kdiff);
         factor=1i*k*deltaz./sqrt(complex(1-(qx.^2+qy.^2)/k^2)).*evanescence_mask;
+        factor(isnan(factor))=0;
     end
-
+    
     function out=window(N)
         %super gaussian window for N
         if gpu
@@ -97,6 +99,6 @@ function exitWave=thibault(wavelength,objects,N,dx,gpu)
         wt = 0.25*N;
         out = exp(-nsq.^8/wt^16);
     end
-
+    
 end
 
