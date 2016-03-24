d2da771e0906da87e157e6c61a15cce05d0adac2
Felix Zimmermann <fzimmermann89@gmail.com>
2016-03-24 20:02:50 +0100

    add comment to rayleigh

--- simulation/rayleigh.m
+++ simulation/rayleigh.m
@@ -1,4 +1,9 @@
 function [theta,intensity]=rayleigh(radius,wavelength,N)
+    % calculate Rayleigh–Debye–Gans intensity profile
+    % of a sphere with given radius at given wavelength in N steps.
+    % returns matrix with angles and matrix with normalized intensities.
+    % see "Patterns in Mie scattering", C.M. Sorensen
+    
     if ~exist('N','var');N=1000;end;
     theta=linspace(0,pi,N);
     q=4*pi/wavelength*sin(theta./2);
