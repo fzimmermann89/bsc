38feb8ed84c7390cc4d3b8a934f5ae38919f43cf
Felix Zimmermann <fzimmermann89@gmail.com>
2016-03-24 19:57:17 +0100

    cleanup msft

--- simulation/msft2.m
+++ simulation/msft2.m
@@ -5,7 +5,7 @@ function out=msft2(wavelength,objects,N,dx,gpu)
     %output is matrix of complex amplitudes over different exit k_x,y (not scatter vectors)
     %constant phase shifts are ignored
     % wavelength (in nm),objects (cell array),N,dx,distanceDetektor,gpu (bool use gpu),debug (bool show progress)
-
+    
     %scattering
     % kout=kin+dk
     % kin_z=k
@@ -15,7 +15,7 @@ function out=msft2(wavelength,objects,N,dx,gpu)
     % kout_x^2+kout_y^2+kout_z^2=k^2
     % <=>kout_z^2=k^2-(kout_x^2+kout_y^2)=k^2-(dk_x^2+dk_y^2)
     % (wave impuls conservation)
-
+    
     %phase:
     % distz: distance from leftmost z
     % phase_before=z*kin_z
@@ -24,26 +24,27 @@ function out=msft2(wavelength,objects,N,dx,gpu)
     % phase=phase_before+phase_after=(maxz-z)*kout_z+distz*kin_z=maxz*out+(z*kin_z-z*kout_z)
     % const phase of maxz*kout (and others) will be neglected.
     % -> phaseshift=z*(k-kout_z)=z*kdiff with
-    % kdiff=k-sqrt(k-(dk_x^2+dk_y^2))
+    % kdiff=k-sqrt(k^2-(dk_x^2+dk_y^2))
+    
     deltaz=dx/2;
     k=2*pi/wavelength;
     Lz=dx*N/2; %cubic box
-
+    
     if gpu
         zero=@()gpuArray.zeros(N,N);
     else
         zero=@()zeros(N,N);
     end
-
+    
     %initialize output
     out=zero();
-
-
+    
+    
     %prepare Objects and retrieve slice functions
     for nobj=length(objects):-1:1
         slicefun{nobj}=objects{nobj}.prepareSliceMethod(N,dx,gpu);
     end
-
+    
     %prepare kin_z-kout_z matrix
     if gpu
         dk_range=gpuArray.linspace(-N/2,N/2-1,N)*(2*pi/(dx*N));
@@ -52,39 +53,43 @@ function out=msft2(wavelength,objects,N,dx,gpu)
     end
     [dk_x,dk_y]=meshgrid(dk_range);
     kdiff=k-real(sqrt(complex(k^2-(dk_x.^2+dk_y.^2))));
-
+    
     %prepare mask for evanescence ("no remaining forwards k")
     mask=k^2>(dk_x.^2+dk_y.^2);
+    
+    %for absorption
     interactionSum=zero();
     for z=-Lz/2:deltaz:Lz/2
-
+        
         %get slices
         dnSlice=zero();
         for nobj=1:length(objects)
             bd=-objects{nobj}.delta+1i*objects{nobj}.beta;
             dnSlice=dnSlice+slicefun{nobj}(z)*bd;
         end
-
-
+        
+        
         if any(dnSlice(:))
-%             %calculate contribution of slice
-%             contrib=ft2(dnSlice)*(dx^2);
-%             %for absoprtion
-%             interactionSum=interactionSum+dnSlice;
-%             absorption=exp(1i*deltaz*k*interactionSum);
-%             %add with correct phase shift
-%             phaseshift=exp(1i*z*kdiff);
-%             out=out+contrib.*phaseshift.*mask.*absorption;
+            %             principle:
+            %             %calculate contribution of slice
+            %             contrib=ft2(dnSlice)*(dx^2);
+            %             %for absoprtion
+            %             interactionSum=interactionSum+dnSlice;
+            %             absorption=exp(1i*deltaz*k*interactionSum);
+            %             %add with correct phase shift
+            %             phaseshift=exp(1i*z*kdiff);
+            %             out=out+contrib.*phaseshift.*mask.*absorption;
+            %             this is combinded to save memory to:
             out=out+ft2(dnSlice)*(dx^2).*exp(1i*z*kdiff+1i*deltaz*k*interactionSum).*mask;
         end
     end
-
-
+    
+    
     %unlock objects
     for nobj=length(objects):-1:1
         objects{nobj}.unlock();
     end
-
-
+    
+    
 end
 
