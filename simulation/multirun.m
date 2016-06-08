478c190a1720abdbe3b768bdf62914f19a170f61
Felix Zimmermann <fzimmermann89@gmail.com>
2016-06-08 19:50:55 +0200

    use structarray instead of cellarray

--- simulation/multirun.m
+++ simulation/multirun.m
@@ -1,97 +1,54 @@
-% load('dens.mat')
 clear all;
 N=2*1024;
+g=gpuDevice();
+
 %all units are in nm
 wavelength=1;
 dx=wavelength;
 dz=dx/2;
-% radius=@(n)5+n/2;
-
-% nruns=40;
-% runs=cell(nruns,1); %saving all
 
 objects=cell(1);
 objects{1}=scatterObjects.sphere();
 
+radiussteps=2.5.*(1:32);
+betasteps=10.^-(1+0.5*(0:8));
+deltasteps=10.^-(1+0.5*(0:8));
 
-n=0;
-nmax=(9*9*32);
+nmax=(numel(radiussteps)*numel(betasteps)*numel(deltasteps));
+n=nmax;
 
-beta=cell(nmax,1);
-delta=cell(nmax,1);
-radius=cell(nmax,1);
-profiles_max=cell(nmax,1);
-profiles_min=cell(nmax,1);
-profiles_stdev=cell(nmax,1);
-profiles_y=cell(nmax,1);
-profiles_x=cell(nmax,1);
-profile_mie=cell(nmax,1);
-errors_abs=cell(nmax,1);
-
-
-for nradius=1:32
-    for nbeta=0:8
-        for ndelta=0:8
+for nradius=1:numel(radiussteps)
+    for nbeta=1:numel(betasteps) 
+        for ndelta=1:numel(deltasteps)
             
-            n=n+1;
-            cradius=2.5*nradius;
+            cradius=radiussteps(nradius);
+            cbeta=betasteps(nbeta);
+            cdelta=deltasteps(ndelta);
             
-            cbeta=10^-(1+0.5*nbeta);
-            cdelta=10^-(1+0.5*ndelta);
             objects{1}.radius=cradius;
             objects{1}.beta=cbeta;
             objects{1}.delta=cdelta;
             
             currun=singlerun(N,dx,dz,wavelength,objects);
-            profiles_max{n}=currun.profiles_max;
-            profiles_min{n}=currun.profiles_min;
-            profiles_stdev{n}=currun.profiles_stdev;
-            profiles_y{n}=currun.profiles_y;
-            profiles_x{n}=currun.profiles_x;
-            profile_mie{n}=currun.mie;
-            errors_abs{n}=currun.errors_abs;
-            beta{n}=cbeta;
-            delta{n}=cdelta;
-            radius{n}=cradius;
+            wait(g);
             
-            fprintf('\n status %f %% \n\n',n/(nmax)*100);
+            profiles_max(n)=currun.profiles_max;
+            profiles_min(n)=currun.profiles_min;
+            profiles_stdev(n)=currun.profiles_stdev;
+            profiles_y(n)=currun.profiles_y;
+            profiles_x(:,n)=currun.profiles_x;
+            profile_mie(:,n)=currun.mie;
+            errors_abs(n)=currun.errors_abs;
+            beta(n)=cbeta;
+            delta(n)=cdelta;
+            radius(n)=cradius;
+            
+            n=n-1;
+            fprintf('\n status %f %% \n\n',(nmax-n)/(nmax)*100);
         end
     end
-    fname=sprintf('C:\\data\\multirun_nrad1-%d.mat',nradius);
-    save(fname,'beta','delta','radius','profiles_max','profiles_min','profiles_stdev','profiles_y','profiles_x','profile_mie','errors_abs','-v6')
-    
+    %     fname=sprintf('C:\\data\\multirun_nrad1-%d.mat',nradius);
+    %     save(fname,'beta','delta','radius','profiles_max','profiles_min','profiles_stdev','profiles_y','profiles_x','profile_mie','errors_abs','-v6')  
 end
-
-save('C:\data\multirun_profiles.mat','beta','delta','radius','profiles_max','profiles_min','profiles_stdev','profiles_y','profiles_x','profile_mie','errors_abs')
-%
-%
-%
-% %% radius variation
-% tic
-% for n=1:nruns;
-%     objects{1}.radius=radius(n);
-%     currun=singlerun(N,dx,dz,wavelength,objects);
-%     %     runs{n}=currun; %saving all
-%
-%     %saving currun
-%     fname=sprintf('C:\\data\\N%d_l%.2g_dx%.2g_dz%.2g_rad%.2g_b%.2g_d%.2g.mat',N,wavelength,dx,dz,radius(n),objects{1}.beta,objects{1}.delta);
-%     save(fname,'currun','-v7.3');
-%
-%
-%     % auswertung radius
-%     rad(n)=currun.objects{1}.radius;
-%     msft(n)=currun.errors_rms.msft;
-%     multislice(n)=currun.errors_rms.multislice;
-%     thibault(n)=currun.errors_rms.thibault;
-%     ftproj(n)=currun.errors_rms.FTproj;
-%     clear currun;
-%     fprintf('finished run %d / %d \n',n,nruns);
-% end
-% toc
-% figure(1);
-% plot(rad,msft,'x',rad,multislice,'x',rad,thibault,'x',rad,ftproj,'x');
-% legend('msft','multi','thibault','proj');
-%
-% %% saving all
-% % fname=sprintf('C:\data\N%d_l%.2g_dx%.2g_dz%.2g_rad%.2g-%.2g_b%.2g_d%.2g.mat',N,wavelength,dx,dz,radius(1),radius(nruns),objects{1}.beta,objects{1}.delta);
-% % save(fname,'runs','-v7.3');
+fname=sprintf('C:\\data\\multirun_profiles_bdr-%s.mat',datestr(datetime('now'),'yyMMdd-HHmm'));
+save(fname','beta','delta','radius','profiles_max','profiles_min','profiles_stdev','profiles_y','profiles_x','profile_mie','errors_abs')
--- simulation/multirun_dx_dz.m
+++ simulation/multirun_dx_dz.m
@@ -1,7 +1,7 @@
-% load('dens.mat')
 clear all;
-N=4*1024;
+N=.25*1024;
 g=gpuDevice();
+
 %all units are in nm
 wavelength=1;
 
@@ -11,42 +11,35 @@ objects{1}.beta=1e-3;
 objects{1}.delta=1e-3;
 objects{1}.radius=50;
 
-n=0;
-nmax=(5*9);
+dxsteps=2.^(-1:4);
+dzsteps=2.^(-4:4);
 
-dx=cell(nmax,1);
-dz=cell(nmax,1);
+nmax=(numel(dxsteps)*numel(dzsteps));
+n=nmax;
 
-profiles_max=cell(nmax,1);
-profiles_min=cell(nmax,1);
-profiles_stdev=cell(nmax,1);
-profiles_y=cell(nmax,1);
-profiles_x=cell(nmax,1);
-profile_mie=cell(nmax,1);
-errors_abs=cell(nmax,1);
-n=27; %for restarating..
-for ndx=2:4 %-1:4
-    for ndz=-4:4
-        
-        n=n+1;
-        
-        cdx=2^ndx;
-        cdz=2^ndz;
+for ndx=1:numel(dxsteps) %-1:4
+    for ndz=1:numel(dzsteps);
+      
+        cdx=dxsteps(ndx);
+        cdz=dzsteps(ndz);
         
         currun=singlerun(N,cdx,cdz,wavelength,objects);
         wait(g);
-        profiles_max{n}=currun.profiles_max;
-        profiles_min{n}=currun.profiles_min;
-        profiles_stdev{n}=currun.profiles_stdev;
-        profiles_y{n}=currun.profiles_y;
-        profiles_x{n}=currun.profiles_x;
-        profile_mie{n}=currun.mie;
-        errors_abs{n}=currun.errors_abs;
-        dx{n}=cdx;
-        dz{n}=cdz;
         
-        fprintf('\n status %f %% \n\n',n/(nmax)*100);
+        profiles_max(n)=currun.profiles_max;
+        profiles_min(n)=currun.profiles_min;
+        profiles_stdev(n)=currun.profiles_stdev;
+        profiles_y(n)=currun.profiles_y;
+        profiles_x(:,n)=currun.profiles_x;
+        profile_mie(:,n)=currun.mie;
+        errors_abs(n)=currun.errors_abs;
+        dx(n)=cdx;
+        dz(n)=cdz;
+        
+        n=n-1;
+        fprintf('\n status %f %% \n\n',(nmax-n)/(nmax)*100);
     end
 end
 
-save('C:\data\multirun_profiles_dx_dz.mat','dx','dz','profiles_max','profiles_min','profiles_stdev','profiles_y','profiles_x','profile_mie','errors_abs')
+fname=sprintf('C:\\data\\multirun_profiles_dxdz-%s.mat',datestr(datetime('now'),'yyMMdd-HHmm'));
+save(fname','beta','delta','radius','profiles_max','profiles_min','profiles_stdev','profiles_y','profiles_x','profile_mie','errors_abs')
