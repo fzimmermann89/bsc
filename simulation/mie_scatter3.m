f022da2257a44ab6019a5c4b2977837bd055f66e
Felix Zimmermann <fzimmermann89@gmail.com>
2016-06-19 23:56:00 +0200

    oversampled mie v1

---
+++ simulation/mie_scatter3.m
@@ -0,0 +1,47 @@
+function out=mie_scatter3(wavelength,radius,beta,delta)
+    N=2048;
+    dx=0.5;
+    
+    
+    y=-N/2-.5:.5:N/2-.5;
+    dscale=(wavelength/(N*dx));
+    out=zeros(N,N);
+    
+    %calculate columns
+    left=mierow(-N/2-.5);
+    for ncol=1:N/2+1;
+        cur=mierow(-N/2+ncol-1);
+        right=mierow(-N/2+ncol-1+.5);
+        tmp=row(left,cur,right);
+        out(:,ncol)=tmp;
+        left=right;
+    end
+    
+   %mirror 
+    for ncol=2:N/2
+        out(:,N-ncol+2)=out(:,ncol);
+    end
+    
+    
+    
+    function out=mierow(x)
+        d=sqrt(x^2+y.^2)*dscale;
+        d=asin(min(1,d));
+        [~,out]=mie(wavelength,radius,beta,delta,d);
+    end
+    
+    function out=row(left,cur,right)
+        out=zeros(N,1);
+        
+        for n=1:N/2+1
+            %weighted average
+            out(n)=(left(2*n-1)+right(2*n-1)+left(2*n+1)+right(2*n+1))/16 ...
+                +(left(2*n)+right(2*n)+cur(2*n-1)+cur(2*n+1))/8 ...
+                +(cur(2*n)/4);
+        end
+        %mirror
+        for n=2:N/2
+            out(N-n+2)=out(n);
+        end
+    end
+end
