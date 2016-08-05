d6b11b4a29bcfae8bf17a63b7d5fde143a2f5425
Felix Zimmermann <fzimmermann89@gmail.com>
2016-08-05 15:27:51 +0200

    reduce memory usage

--- recon/pad2size.m
+++ recon/pad2size.m
@@ -1,12 +1,17 @@
-function output=pad2size(input,tosize)
+function output=pad2size(input,tosize,val)
     tosize=[0,0]+tosize;
-    output=zeros(tosize).*input(1,1);
+    if nargin<3||val==0
+        output=zeros(tosize,'like',input);
+    else
+      output=zeros(tosize,'like',input)+val;  
+    end
+    
     diff=tosize-size(input);
     if ~(diff>0)
         error('shrinking not supported')
     end
     pad=ceil(diff/2);
-    idx=1+pad(1):pad(1)+size(input,1);
-    idy=1+pad(2):pad(2)+size(input,2);
-    output(idx,idy)=input;
+%     idx=1+pad(1):pad(1)+size(input,1);
+%     idy=1+pad(2):pad(2)+size(input,2);
+    output(1+pad(1):pad(1)+size(input,1),1+pad(2):pad(2)+size(input,2))=input;
 end
