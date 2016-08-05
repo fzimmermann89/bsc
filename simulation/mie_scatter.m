1315809f115ad7454ea492adecb9884b3f1725b1
Felix Zimmermann <fzimmermann89@gmail.com>
2016-08-06 00:32:40 +0200

    format

--- simulation/mie_scatter.m
+++ simulation/mie_scatter.m
@@ -1,32 +1,22 @@
 function out=mie_scatter(wavelength,radius,beta,delta,N,dx,offset)
+    %Calculate a 2D scatter image using mie. Uses symmetry to save memory.
+    
     if nargin<7;offset=0;end
-    center=N/2+1+offset;
     out=zeros(N,N);
-%     tic
-%     range=linspace(-N/2+1+offset,offset,N/2);
-%     [XX,YY]=meshgrid(range,range);
-%     R=(hypot(XX,YY));
-%      angle=asin(min(1,(R/(N*dx)*wavelength)));
-%       [uangle,~,iangle]=unique(angle,'sorted');
-%     toc
     
-    tic
-    range=linspace(-N/2+offset,offset,N/2+1).^2;
-    [XX,YY]=meshgrid(range,range);
-    R=(sqrt(XX+YY));
-    [uR,~,idx]=unique(R,'sorted');
+    %calculate unique angles in first quadrant
+        range=linspace(-N/2+offset,offset,N/2+1).^2;
+        [XX,YY]=meshgrid(range,range);
+        R=(sqrt(XX+YY));
+        [uR,~,idx]=unique(R,'sorted');
         angle=asin(min(1,(uR/(N*dx)*wavelength)));
-  [~,mieval]=mie(wavelength,radius,beta,delta,angle);
-  mieval=reshape(mieval(idx),[N/2+1,N/2+1]);
-  out(1:N/2+1,1:N/2+1)=mieval;
-  out(N/2+2:end,1:N/2+1)=flipud(mieval(2:end-1,1:end));
-  out(1:end,end/2+2:end)=fliplr(out(1:end,2:end/2));
-    toc
+    %mie scattering for first quadrant using the unique angles
+        [~,mieval]=mie(wavelength,radius,beta,delta,angle);
+        mieval=reshape(mieval(idx),[N/2+1,N/2+1]);
     
-%     angle=asin(min(1,(R/(N*dx)*wavelength)));
-%     [uangle,~,iangle]=unique(angle,'sorted');
-%     [~,mieval]=mie(wavelength,radius,beta,delta,uangle);
-%     out=reshape(mieval(iangle),[N,N]);
-% %     out=out./out(end/2+2,end/2+2);
+    %set first quadrant
+        out(1:N/2+1,1:N/2+1)=mieval;
+    %mirror for other quadrants
+        out(N/2+2:end,1:N/2+1)=flipud(mieval(2:end-1,1:end));
+        out(1:end,end/2+2:end)=fliplr(out(1:end,2:end/2));
 end
-% mie_scatter(wavelength,radius,beta,delta,4*N,dx)
