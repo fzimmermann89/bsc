967841459887c8aaaa243de7aaa149bfc64571a7
Felix Zimmermann <fzimmermann89@gmail.com>
2016-08-16 20:59:35 +0200

    test msft-absorption and ftprojection-exp, use gpu
    Former-commit-id: d510fbf48bf4e52eaebf65a977c127e776c732d9

--- simulation/singlerun2.m
+++ simulation/singlerun2.m
@@ -1,7 +1,6 @@
 
-function run=singlerun2(N,dx,dz,wavelength,objects)
+function run=singlerun2(N,dx,dz,wavelength,objects,simabs)
     % runs all algorithms for a single set of parameters
-    
     %% setup
     padhalf=true;
     padcut=false;
@@ -13,6 +12,7 @@ function run=singlerun2(N,dx,dz,wavelength,objects)
     try
         gpu=parallel.gpu.GPUDevice.isAvailable;
         g=gpuDevice();
+        reset(g);
     catch
         gpu=false;
     end
@@ -27,67 +27,73 @@ function run=singlerun2(N,dx,dz,wavelength,objects)
     
     
     %% mie
-    imie=mie_scatter(wavelength,objects{1}.radius,objects{1}.beta,objects{1}.delta,2*N,dx);
+    imie=gpuArray(mie_scatter(wavelength,objects{1}.radius,objects{1}.beta,objects{1}.delta,2*N,dx));
     imie=normalize2(imie);
     imie=halfimage(imie);
     [~,pmie]=rprofil(imie,length(imie)/2);
-    run.mie=imie;
-    run.profile_mie=pmie;
+    run.mie=gather(imie);
+    run.profile_mie=gather(pmie);
+    clear pmie;
     
     %% ft projection
-    exitwave=(abs(scatterObjects.projection(objects,N,dx,gpu)));
+%     exitwave=exp(2i*pi*dx/wavelength*scatterObjects.projection(objects,N,dx,gpu)); %absorption
+   exitwave=scatterObjects.projection(objects,N,dx,gpu); %direct projection
+%   exitwave=scatterObjects.projection(objects,N,dx,gpu)~=0; % outline
+
     name='FTproj';
     run=addtorun(run,name,exitwave);
     clear exitwave;
-    if gpu;wait(g);end;
+%     if gpu;wait(g);end;
     
     %% multislice
     exitwave=(multislice(wavelength,objects,N,dx,dz,gpu,false));
     name='multislice';
     run=addtorun(run,name,exitwave);
     clear exitwave;
-    if gpu;wait(g);end;
+%     if gpu;wait(g);end;
     
     %% thibault
     exitwave=(thibault(wavelength,objects,N,dx,dz,gpu));
     name='thibault';
     run=addtorun(run,name,exitwave);
     clear exitwave;
-    if gpu;wait(g);end;
+%     if gpu;wait(g);end;
     
     %% msft
-    msft=(msft2(wavelength,objects,N,dx,dz,gpu));
+    msft=(msft2(wavelength,objects,N,dx,dz,gpu,simabs));
     exitwave=ift2(msft/(dx^2));
     clear msft;
     name='msft';
     run=addtorun(run,name,exitwave);
     clear exitwave;
-    if gpu;wait(g);end;
+%     if gpu;wait(g);end;
     
     %% status
     err=structfun(@(x)median(abs(x(angles>minangle&angles<maxangle))),run.error_rel,'UniformOutput',false);
-    fprintf('\n Done with N%g, dx%g, dz%g. Time: %gs. Median Errors:\n',N,dx,dz,toc);
+    fprintf('\n Done with b%.2e, d%.2e, rad%g (N%g, dx%g, dz%g) Time: %gs. Median Errors:\n',objects{1}.beta,objects{1}.delta,objects{1}.radius,N,dx,dz,toc);
     disp(struct2table(err));
     fprintf('\n\n');
     
     %% helper functions
     function run=addtorun(run,name,exitwave)
-        exitwave=gather(exitwave);
+        %exitwave=gather(exitwave);
+       
         scatter=exitwave2scatter(exitwave,dx,wavelength,padhalf,padcut,angles);
-        scatter=icorrectoffsetspan(scatter,imie,angles>minangle&angles<maxangle/2);
+        scatter=icorrectoffsetspan2(scatter,imie,angles>minangle&angles<maxangle/2);
         
         error_abs=scatter-imie;
         error_rel=(error_abs)./imie;
-        [~,rerror_rel]=rprofil(error_rel,N/2);
         [~,rerror_abs]=rprofil(error_abs,N/2);
+        [~,rerror_rel]=rprofil(error_rel,N/2);
         [~,rscatter]=rprofil(scatter,N/2);
-        run.exitwave.(name)=exitwave;
-        run.scatter.(name)=scatter;
-        run.error_rel.(name)=error_rel;
-        run.error_abs.(name)=error_abs;
-        run.profile_error_rel.(name)=rerror_rel;
-        run.profile_error_abs.(name)=rerror_abs;
-        run.profile_scatter.(name)=rscatter;
+        
+        run.exitwave.(name)=gather(exitwave);
+        run.scatter.(name)=gather(scatter);
+        run.error_rel.(name)=gather(error_rel);
+        run.error_abs.(name)=gather(error_abs);
+        run.profile_error_rel.(name)=gather(rerror_rel);
+        run.profile_error_abs.(name)=gather(rerror_abs);
+        run.profile_scatter.(name)=gather(rscatter);
         clear scatter error_abs error_rel rerror_rel rerror_abs rscatter
         if gpu;wait(g);end;
     end
