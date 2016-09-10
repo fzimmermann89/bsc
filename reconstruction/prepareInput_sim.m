b0dc60c666896791cdbce39064c24b0384643180
Felix Zimmermann <fzimmermann89@gmail.com>
2016-09-10 11:24:05 +0200

    recon: change noise and image composition

--- reconstruction/prepareInput_sim.m
+++ reconstruction/prepareInput_sim.m
@@ -1,21 +1,24 @@
-function [scatterImageHolo,refImage,mask,softmask,outermask,scatterImage]=prepareInput_sim(filename,refRadius,refError,maskScale,sigmaMask,discreteBits)
+function [scatterImageHolo,scatterImage,refImage,mask,softmask,outermask,inputHolo,input]=prepareInput_sim(filename,refRadius,refError,maskScale,sigmaMask,discreteBits)
     
     %load image
     input=double((rgb2gray(imread(filename))))/255;
-    input=input-input(1);
+    input=(input-min(input(:)))./max(input(:));
     N=size(input);
     
+
     %create Reference
     [xx,yy]=meshgrid(-refRadius:1:refRadius);
     realRefImage=xx.^2+yy.^2<refRadius^2;
-    inputHolo=input;
-    inputHolo(end-4*refRadius:end-2*refRadius,end-4*refRadius:end-2*refRadius)=realRefImage;
+ 
+    offset=[refRadius,refRadius];%Npadded/8;
+    inputHolo=padarray(input,ceil(N),0,'post');
     
-    %pad
-    inputHolo=padarray(inputHolo,ceil(size(inputHolo)/2),0);
-    Npadded=size(inputHolo);
-    input=padarray(input,ceil(size(input)/2),0);
-
+    inputHolo((end-2*refRadius:end)-offset(1),(end-2*refRadius:end)-offset(2))=realRefImage;
+    inputHolo=padarray(inputHolo,2.^nextpow2(1.5*size(inputHolo))-size(inputHolo),0,'post');
+    
+    input=padarray(input,(size(inputHolo)-N)/2,0,'both');
+    Npadded=size(input);
+    figure(9); imagesc(abs(ift2(abs(ft2(inputHolo)).^2)));
     %create masks
     [ mask,softmask,outermask ]=circularMask(Npadded,maskScale,sigmaMask);
     
@@ -25,19 +28,19 @@ function [scatterImageHolo,refImage,mask,softmask,outermask,scatterImage]=prepar
     
     %discretize and noise
     if (discreteBits~=0)
-        dynamicRange=max(scatterImageHolo(:))-min(scatterImageHolo(scatterImageHolo~=0));
-        rangePerBit=dynamicRange./2^discreteBits;
-        scatterImageHolo=uint16(round(scatterImageHolo./rangePerBit));
-        scatterImageHolo=double(imnoise(scatterImageHolo,'poisson'));
+        scale=(max(scatterImageHolo(:))-min(scatterImageHolo(scatterImageHolo~=0)))/(1e-12*2^discreteBits);
+        scatterImageHolo=scatterImageHolo./scale;
+        scatterImageHolo=round(10^12*imnoise(double(scatterImageHolo),'poisson'));
         
-        dynamicRange=max(scatterImage(:))-min(scatterImage(scatterImage~=0));
-        rangePerBit=dynamicRange./2^discreteBits;
-        scatterImage=uint16(round(scatterImage./rangePerBit));
-        scatterImage=double(imnoise(scatterImage,'poisson'));
+        scale=(max(scatterImage(:))-min(scatterImage(scatterImage~=0)))/(1e-12*2^discreteBits);
+        scatterImage=scatterImage./scale;
+        scatterImage=round(10^12*imnoise(double(scatterImage),'poisson'));
+        
+        
     end
     
     %create guessed Reference
     [xx,yy]=meshgrid(-refRadius*refError:1:refRadius*refError);
     refImage=xx.^2+yy.^2<(refRadius*refError)^2;
-   
+    
 end
