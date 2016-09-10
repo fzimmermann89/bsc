d7e17e789661326b96ef5308cf32ac9ff093d4cf
Felix Zimmermann <fzimmermann89@gmail.com>
2016-09-10 11:24:33 +0200

    fig recon

---
+++ reconstruction/fig_recon.m
@@ -0,0 +1,158 @@
+clear all;
+close all;
+
+addpath('reconstruction');
+g=gpuDevice(1);
+
+%% settings
+refRadius=40;
+refError=1.05;
+maskScale=16/2048;%32%64 %.02%.03%.1%.03%.03%.1%0.03%.1;
+sigmaMask=24;%32;
+discreteBits=16;%15;
+wienernoise=1000;%100;
+caption='mask16bit16error05';
+outpath='./Tex/images';
+inputfilename='./reconstruction/input/input_tu2.png';
+outputfilename=sprintf('%s/recon2d-%s.png',outpath,caption);
+
+%% Prepare Input
+[scatterImageHolo,scatterImage,refImage,mask,softmask,outermask,inputHolo,input]=prepareInput_sim(inputfilename,refRadius,refError,maskScale,sigmaMask,discreteBits);
+%move to gpu
+mask=gpuArray(mask);
+scatterImageHolo=gpuArray(scatterImageHolo);
+scatterImage=gpuArray(scatterImage);
+
+
+
+%% use Holography and IPR
+%support and start
+[start,support]=holoSupport(scatterImageHolo,softmask,refImage,'threshold',.5,'debug',true);
+
+%Plan:
+planHolo=recon.plan();
+for n=1:60
+    planHolo.addStep('hio',300);
+    planHolo.addStep('er',1);
+    planHolo.addStep('show');
+end
+planHolo.addStep('er',50);
+planHolo.addStep('show');
+
+%Run
+[resultHolo]=planHolo.run(scatterImageHolo,support,start,mask);
+
+
+%% use IPR with Shrinkwrap
+%support and start
+[start,support]=genericSupport(scatterImage,softmask);
+
+%Plan
+planSW=recon.plan();
+
+for n=1:60
+    planSW.addStep('hio',95);
+    planSW.addStep('errp',5);
+    planSW.addStep('sw',1,[5,0.025]);
+    planSW.addStep('show')
+end
+
+planSW.addStep('loosen',1,5)
+planSW.addStep('show')
+
+for n=1:30
+    planSW.addStep('hio',300);
+    planSW.addStep('er',1);
+     planSW.addStep('show');
+end
+planSW.addStep('er',50);
+planSW.addStep('show');
+
+%Run multiple times and average
+%         multi=10;
+%         multistart=zeros([size(start),multi]);
+%         for n=1:multi
+%             multistart(:,:,n)=gather(ift2(ft2(start).*exp(2i*pi*rand(size(start))).*softmask));
+%         end
+%         [result,images,errors]=planSW.runAvg(scatterImage,support,multistart,mask,ceil(multi/2));
+[resultSW]=planSW.run(scatterImage,support,start,mask);
+
+figure(999);
+fresultSW=maskfilter(abs(resultSW),abs(softmask));
+imagesc(abs(fresultSW));
+colormap(flipud(colormap(gray)));
+drawnow;
+%% wiener deconvolution
+%get cross correlation
+
+[~,~,cross]=holoSupport(scatterImageHolo,softmask,refImage,'threshold',.2,'radDilate',20);
+%and filtered (guessed) Reference
+refImageFiltered=maskfilter(refImage,softmask,2.^nextpow2(size(refImage)*4));
+crossPadded=pad2size(cross,size(scatterImageHolo));
+refImagePadded=pad2size(refImageFiltered,size(scatterImageHolo));
+%deconvolution
+resultDeconv=wiener(crossPadded,refImagePadded,wienernoise);
+
+figure(wienernoise);
+fresultDeconv=maskfilter(resultDeconv,softmask);
+imagesc(abs(fresultDeconv));
+colormap(flipud(colormap(gray)));
+drawnow;
+%% plot results
+finput=maskfilter(input,softmask);
+fresultSW=maskfilter(resultSW,softmask);
+fresultHolo=maskfilter(resultHolo,softmask);
+fresultDeconv=maskfilter(resultDeconv,softmask);
+
+move=@(x)moveAndMirror(finput,x);
+cut=@(x)x(end/2-end/8:end/2+end/8+1,end/2-end/8:end/2+end/8+1);
+
+
+f=figure();
+delim=32;
+pixel=512;
+scale=1/2;
+cmin=min(abs(finput(:)));
+cmax=max(abs(finput(:)));
+f.Position=[0,0,(2*pixel+delim),(2*pixel+delim)].*scale;
+ax(1)=subplot(2,2,1);
+ax(1).Units='pixels';
+ax(1).Position=[0,pixel+delim,pixel,pixel].*scale;
+imagesc(abs(cut(finput)));
+colormap(flipud(colormap(gray)))
+caxis([cmin,cmax]);
+axis off;
+ax(1).ActivePositionProperty='position';
+
+ax(2)=subplot(2,2,3);
+ax(2).Units='pixels';
+ax(2).Position=[0,0,pixel,pixel].*scale;
+imagesc(abs(cut(move(fresultDeconv))));
+colormap(flipud(colormap(gray)))
+axis off;
+ax(2).ActivePositionProperty='position';
+
+ax(3)=subplot(2,2,2);
+ax(3).Units='pixels';
+ax(3).Position=[pixel+delim,pixel+delim,pixel,pixel].*scale;
+imagesc(abs(cut(move(fresultSW))));
+colormap(flipud(colormap(gray)))
+caxis([max(cmin,min(abs(fresultSW(:)))),min(cmax,max(abs(fresultSW(:))))]);
+axis off;
+ax(3).ActivePositionProperty='position';
+
+ax(4)=subplot(2,2,4);
+ax(4).Units='pixels';
+ax(4).Position=[pixel+delim,0,pixel,pixel].*scale;
+colormap(flipud(colormap(gray)))
+imagesc(abs(cut(move(fresultHolo))));
+caxis([max(cmin,min(abs(fresultHolo(:)))),min(cmax,max(abs(fresultHolo(:))))]);
+axis off;
+ax(4).ActivePositionProperty='position';
+
+f.PaperUnits='inches';
+f.PaperPositionMode='manual';
+f.PaperPosition=[0,0,(2*pixel+delim)/150,(2*pixel+delim)/150];
+f.PaperSize=[(2*pixel+delim)/150, (2*pixel+delim)/150];
+f.Resize='off';
+ print(outputfilename,'-dpng','-r150');
--- reconstruction/test_recon.m
+++ reconstruction/test_recon.m
@@ -1,16 +1,20 @@
 clear all;
 g=gpuDevice(1);
-refRadius=20;
-refError=1.02;
-maskScale=0/1024;%.02%.03%.1%.03%.03%.1%0.03%.1;
-sigmaMask=32;%32;
-discreteBits=0;%15;
-inputfilename='./input/input_2.png';
-ToDo='holo'; %possible cases: ToDo='holo'; ToDo='sw'; ToDo='deconv'
+refRadius=40;
+refError=1.05;
+maskScale=32/2048;%32%64 %.02%.03%.1%.03%.03%.1%0.03%.1;
+sigmaMask=24;%32;
+discreteBits=15;%15;
+inputfilename='./input/input_tu2.png';
+ToDo='deconv'; %possible cases: ToDo='holo'; ToDo='sw'; ToDo='deconv'
+
 
 %Prepare Input
-[scatterImageHolo,refImage,mask,softmask,outermask,scatterImage]=prepareInput_sim(inputfilename,refRadius,refError,maskScale,sigmaMask,discreteBits);
+[scatterImageHolo,scatterImage,refImage,mask,softmask,outermask,inputHolo,input]=prepareInput_sim(inputfilename,refRadius,refError,maskScale,sigmaMask,discreteBits);
+%move to gpu
 mask=gpuArray(mask);
+scatterImageHolo=gpuArray(scatterImageHolo);
+scatterImage=gpuArray(scatterImage);
 
 
 %Reconstruct
@@ -24,9 +28,10 @@ switch ToDo
         for n=1:40
             planHolo.addStep('hio',300);
             planHolo.addStep('er',1);
+            %planHolo.addStep('sw',1,[8,0.01]);
             planHolo.addStep('show');
         end
-        planHolo.addStep('er',100);
+        planHolo.addStep('er',50);
         planHolo.addStep('show');
         
         %Run
@@ -35,33 +40,51 @@ switch ToDo
         
     case 'sw' %use IPR with Shrinkwrap
         %support and start
-        [initialstart,support]=genericSupport(scatterImage,softmask);
+        [start,support]=genericSupport(scatterImage,softmask);
         
-        for n=1:5
-            start(:,:,n)=ift2(ft2(initialstart).*exp(2i*pi*rand(size(initialstart))).*softmask);
-        end
+        
         
         %Plan
         planSW=recon.plan();
         
-        for n=1:50
-            planSW.addStep('hio',45);
+        for n=1:60
+            planSW.addStep('hio',95);
             planSW.addStep('errp',5);
-            planSW.addStep('sw',1,[4,0.08]);
+            planSW.addStep('sw',1,[5,0.05]);
+            planSW.addStep('show')
         end
-        planSW.addStep('loosen',1,5)
+        
+        planSW.addStep('loosen',1,10)
         planSW.addStep('show')
         
-        for n=1:50
-            planSW.addStep('hio',150);
+        %         for n=1:30
+        %             planSW.addStep('errp',5);
+        %             planSW.addStep('hio',95);
+        %             planSW.addStep('sw',1,[8,0.075]);
+        %             planSW.addStep('show')
+        %         end
+        %
+        %         planSW.addStep('errp',20);
+        %         planSW.addStep('sw',1,[5,0.05]);
+        %         planSW.addStep('loosen',1,10)
+        %         planSW.addStep('show')
+        
+        for n=1:20
+            planSW.addStep('hio',300);
             planSW.addStep('er',1);
+            planSW.addStep('show');
         end
         planSW.addStep('er',50);
         planSW.addStep('show');
         
         %Run
-        [result,images]=planSW.runAvg(scatterImage,support,start,mask);
-        %[avg,images,errors]=planSW.runAvg(scatterImage,support,start,mask);
+        %         multi=10;
+        %         multistart=zeros([size(start),multi]);
+        %         for n=1:multi
+        %             multistart(:,:,n)=gather(ift2(ft2(start).*exp(2i*pi*rand(size(start))).*softmask));
+        %         end
+        %         [result,images,errors]=planSW.runAvg(scatterImage,support,multistart,mask,ceil(multi/2));
+        [result,images]=planSW.run(scatterImage,support,start,mask);
         
         
     case 'deconv' %wiener deconvolution
@@ -72,7 +95,7 @@ switch ToDo
         crossPadded=pad2size(cross,size(scatterImageHolo));
         refImagePadded=pad2size(refImageFiltered,size(scatterImageHolo));
         %deconvolution
-        result=wiener(crossPadded,refImagePadded,10);
+        result=wiener(crossPadded,refImagePadded,100);
         
         
     otherwise
