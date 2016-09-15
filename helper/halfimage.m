f46bebfaa5ac6e086d6dcc74d2b58df47dd9dd25
Felix Zimmermann <fzimmermann89@gmail.com>
2016-09-16 00:15:38 +0200

    halfimage:complex

--- helper/halfimage.m
+++ helper/halfimage.m
@@ -1,19 +1,23 @@
 function out=halfimage(in)
     persistent kernel;
-    try
+     try
         N=floor(length(in)/2);
         if isempty(kernel)||~existsOnGPU(kernel)
             kernel = parallel.gpu.CUDAKernel('halfimage.ptx','halfimage.cu','halfimage');
         end
         kernel.ThreadBlockSize =[4, 8,1];
         kernel.GridSize=[ceil(N/4),ceil(N/8),1];
-        
-        out=feval(kernel,N,gpuArray(double(in)),gpuArray.zeros(N,N));
+        if isreal(in);
+            out=feval(kernel,N,gpuArray(double(in)),gpuArray.zeros(N,N));
+        else
+            out=feval(kernel,N,gpuArray(double(real(in))),gpuArray.zeros(N,N));
+            out=out+1i*feval(kernel,N,gpuArray(double(imag(in))),gpuArray.zeros(N,N));
+        end
         if ~isa(in,'gpuArray')
             out=gather(out);
         end;
-    catch
-        kernel=[];
+     catch
+        clear kernel;
         error('error running halfimage kernel');
     end
 end
