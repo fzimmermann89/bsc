498367a0709eca248fd03fddc6afe9d08df04109
Felix Zimmermann <fzimmermann89@gmail.com>
2016-09-16 00:15:12 +0200

    inner and outermask should always be logical

--- helper/circularMask.m
+++ helper/circularMask.m
@@ -2,9 +2,9 @@ function [ innermask,softmask,outermask ] = circularMask( N,maskScale,sigma )
     %create masks for simulation
     %softmask is gauss filtered mask, outermask is extended to affected area, innermask is unaffected area
     if maskScale==0
-        innermask=ones(N);
+        innermask=true(N);
         softmask=ones(N);
-        outermask=ones(N);
+        outermask=true(N);
     else
         [xx,yy]=meshgrid(-N/2:1:N/2-1);
         innermask=xx.^2+yy.^2>=(maskScale*max(N))^2;
