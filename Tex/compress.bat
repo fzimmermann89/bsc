3366328a36a699ba85f88e8afc3c6143d875175e
Felix <fzimmermann89@gmail.com>
2016-09-24 19:09:22 +0200

    tex: remove cmyk conversion from compress

--- Tex/compress.bat
+++ Tex/compress.bat
@@ -1,3 +1,3 @@
-gs -o compressed.pdf -sDEVICE=pdfwrite -sProcessColorModel=DeviceCMYK -sColorConversionStrategy=CMYK -sColorConversionStrategyForImages=CMYK -dPDFSETTINGS=/prepress -dNOPAUSE -dBATCH main.pdf
+gs -o compressed.pdf -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -dNOPAUSE -dBATCH main.pdf
 rm main.pdf
 mv compressed.pdf main.pdf
