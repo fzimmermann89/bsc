8c98a107ffd617d1d63e96ece55ca5d26a0601bb
Felix Zimmermann <fzimmermann89@gmail.com>
2016-09-28 19:38:39 +0200

    remove playground

--- playground/test3.m
+++
@@ -1,7 +0,0 @@
-xv = [1 4 4 1 1 NaN 2 2 3 3 2];
-yv = [1 1 4 4 1 NaN 2 3 3 2 2];
-
-xq = rand(500,1)*5;
-yq = rand(500,1)*5;
-
-in =  scatterObjects.poly2mat(10,xv,yq,5);
--- playground/test_gpu_3d.m
+++
@@ -1,7 +0,0 @@
-function test_gpu_3d
-    function ret=returnMatrix(z)
-        ret=ones(N,N)*z;
-    end
-testz=gpuArray(1:100);
-test=pagefun(@returnMatrix,testz);
-end
