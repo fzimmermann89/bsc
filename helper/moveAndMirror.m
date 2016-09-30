924c64e38ca359953b31e09d1ee0e892c6c58b6d
Felix Zimmermann <fzimmermann89@gmail.com>
2016-09-30 20:16:17 +0200

    fix moveandmirror abs and supress warnings

--- helper/moveAndMirror.m
+++ helper/moveAndMirror.m
@@ -1,7 +1,12 @@
-function  out  = moveAndMirror( fixed,moving )
+function  [out,didMirror]  = moveAndMirror( fixed,moving )
+    % Moves and mirrors 'moving' to match 'fixed'.
+    % didMirror is logical and true result was mirrored.
+    % Can handle complex and gpu inputs.
+    
+    didMirror=false;
     Nfixed=abs(fixed);
     Nmoving=abs(moving);
-    Nfixed=Nfixed-min(fixed(:));
+    Nfixed=Nfixed-min(Nfixed(:));
     Nfixed=gather(Nfixed./max(Nfixed(:)));
     Nmoving=Nmoving-min(Nmoving(:));
     Nmoving=gather(Nmoving./max(Nmoving(:)));
@@ -9,9 +14,14 @@ function  out  = moveAndMirror( fixed,moving )
     maxXcorr=@(a,b)max(max(abs(ift2(ft2(a).*conj(ft2(b))))));
     
     if maxXcorr(Nfixed,Nrotated)>maxXcorr(Nfixed,Nmoving)
-        moving=rot90(moving,2);
+        moving=(rot90(moving,2));
         Nmoving=rot90(Nmoving,2);
+        didMirror=true;
     end
+    %disable warning for weak correlation because of message spam..
+    w=warning('query','images:imregcorr:weakPeakCorrelation');
+    warning('off',w.identifier);
     out=imwarp(gather(moving),imregcorr(Nmoving,Nfixed,'translation'),'OutputView',imref2d(size(Nfixed)));
+    warning(w.state,w.identifier);
 end
 
