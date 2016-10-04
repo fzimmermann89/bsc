fc3d24fe3c2ec93f0e2714fda1c7e44501c8647e
Felix Zimmermann <fzimmermann89@gmail.com>
2016-10-05 01:01:36 +0200

    genericsupport: enable smaller support, make threshold offset independent

--- reconstruction/genericSupport.m
+++ reconstruction/genericSupport.m
@@ -2,14 +2,14 @@ function [start,support]=genericSupport(scatterImage,softmask,relThreshold)
     %creates a support and start values without using holography
     if nargin<3;relThreshold=0.005;end
    support=abs(ift2(scatterImage.*softmask));
-    support=support>max(support(:))*relThreshold;
+    support=abs(support-support(1))>max(abs(support(:)-support(1)))*relThreshold;
 
     support = imdilate(support,ones(3));
     support = imfill(support, 'holes');
     
-%     st = regionprops(uint16(support), 'BoundingBox' );
-%     support=zeros(size(scatterImage));
-%     support(end/2-st.BoundingBox(4)/4:end/2+st.BoundingBox(4)/4,end/2-st.BoundingBox(3)/4:end/2+st.BoundingBox(3)/4)=1;
+     st = regionprops(uint16(support), 'BoundingBox' );
+     support=zeros(size(scatterImage));
+     support(end/2-st.BoundingBox(4)/4:end/2+st.BoundingBox(4)/4,end/2-st.BoundingBox(3)/4:end/2+st.BoundingBox(3)/4)=1;
 %    
 %     
     
