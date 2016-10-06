db54bcb7ba646e12390056897060b4caaac03ff0
Felix Zimmermann <fzimmermann89@gmail.com>
2016-10-06 13:42:16 +0200

    recon:add option halfbox to genericsupport

--- reconstruction/genericSupport.m
+++ reconstruction/genericSupport.m
@@ -1,17 +1,19 @@
-function [start,support]=genericSupport(scatterImage,softmask,relThreshold)
-    %creates a support and start values without using holography
+function [start,support]=genericSupport(scatterImage,softmask,relThreshold,halfbox)
+    %creates a support and start values without using holography. if halfbox is
+    %true, the support will be half of the boundingbox of the autocorrelation,
+    %otherwise it will be the autocorrelation.
+    
     if nargin<3;relThreshold=0.005;end
-   support=abs(ift2(scatterImage.*softmask));
-    support=abs(support-support(1))>max(abs(support(:)-support(1)))*relThreshold;
-
+    support=abs(ift2(scatterImage.*softmask));
+    support=abs(support-support(1))>max(abs(support(:)-support(1)))*relThreshold;   
     support = imdilate(support,ones(3));
     support = imfill(support, 'holes');
-    
-     st = regionprops(uint16(support), 'BoundingBox' );
-     support=zeros(size(scatterImage));
-     support(end/2-st.BoundingBox(4)/4:end/2+st.BoundingBox(4)/4,end/2-st.BoundingBox(3)/4:end/2+st.BoundingBox(3)/4)=1;
-%    
-%     
+   
+    if nargin>3&&halfbox
+        st = regionprops(uint16(support), 'BoundingBox' );
+        support=zeros(size(scatterImage));
+        support(end/2-st.BoundingBox(4)/4:end/2+st.BoundingBox(4)/4,end/2-st.BoundingBox(3)/4:end/2+st.BoundingBox(3)/4)=1;
+    end
     
     randPhase=2*pi*rand(size(scatterImage));
     start=(ift2(sqrt(scatterImage).*exp(1i*randPhase).*softmask));
