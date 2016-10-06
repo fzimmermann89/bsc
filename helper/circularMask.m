a2ad21e61f748877376393223bb1ccb25e6af3e2
Felix Zimmermann <fzimmermann89@gmail.com>
2016-10-06 13:43:54 +0200

    fix sigma=0 for circularmask

--- helper/circularMask.m
+++ helper/circularMask.m
@@ -9,7 +9,11 @@ function [ innermask,softmask,outermask ] = circularMask( N,maskScale,sigma )
         [xx,yy]=meshgrid(-N/2:1:N/2-1);
         innermask=xx.^2+yy.^2>=(maskScale*max(N))^2;
         tmpmask=xx.^2+yy.^2>=(maskScale*max(N)+2*sigma)^2;
-        softmask=imgaussfilt(double(tmpmask),sigma);
+        if sigma>0
+            softmask=imgaussfilt(double(tmpmask),sigma);
+        else
+            softmask=tmpmask;
+        end
         softmask=softmask.*innermask;
         softmask(softmask>0.99)=1;
         outermask=softmask==1;
