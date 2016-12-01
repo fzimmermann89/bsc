0978e73a05eba7888c0f61b5258400cdbd8d7091
Felix Zimmermann <fzimmermann89@gmail.com>
2016-12-01 17:34:08 +0100

    Reconstruction: redo wiener filter

--- reconstruction/wiener.m
+++ reconstruction/wiener.m
@@ -1,36 +1,64 @@
-function [ output ] = wiener( input,h,noise,normalize )
+function [ output ] = wiener( input,h,noise,ps_signal,normalize )
     %Wiener deconvolution
     %                   H* S_signal
     % G     =  ------------------------------
     %            |H|^2 S_signal + S_noise
-    %(allows complex h)
-    %if normalize (default:false) normalize output to input
-   
-    %ft inputs
-    H = ft2(h);
-    Finput = ft2(input);
-
-    %power spectral density of input image
-    S = abs(Finput).^2;
-
-    %power spectral density of noise
-    if isscalar(noise)
-        N=noise*S;
-    else
-        Fnoise = ft2(noise);
-        N = abs(Fnoise).^2;
+    %
+    %noise can be a scalar value (Noise/Signal Ratio) or a power spectrum.
+    %ps_signal can be the power spectrum of the signal, if not set the
+    %power spectrum of input will be used
+    %if normalize (default:false) normalize deconvolution filter 
+    
+    if any(size(input)~=size(h))
+        error('input and h must have same dimensions')
     end
-
+    
+    %check if 1D or 2D and transform inputs
+    if min([size(input),size(h)])>1
+        is2d=true;
+        if size(input,1)~=size(input,2)||bitand(size(input,1),1)
+            error('inputs must be square matrixes of even length')
+        end   
+        H = ft2(h);
+        Finput = ft2(input);
+    else
+        is2d=false;
+        H = fftshift(fft(ifftshift(h)));
+        Finput = fftshift(fft(ifftshift(input)));
+    end
+    
+    if isscalar(noise)
+        %heuristic wiener filter
+        S=1;
+        N=noise;
+    else
+        %power spectral density of image and noise
+        if ~all(size(noise)==size(input))
+            error('noise can be scalar or a matrix of the same size as input')
+        end
+        N = noise; 
+        if nargin>3&&numel(ps_signal)==numel(N)
+            %use power spectrum of signal
+            S=ps_signal;
+        else
+            S = (abs(Finput).^2);
+        end
+    end
+    
     %wiener filter
-    G =(conj(H).*S)./(abs(H).^2.*S+N);
-
+    G =(conj(H).*S)./max(eps,(abs(H).^2.*S+N));
+    
+    %normalize G
+    if nargin>4&&normalize
+        G=G./sum(abs(G(:)));
+    end
+    
     %deconvolution
     Foutput = Finput.*G;
-    output = ift2(Foutput);
-    
-    %normalize
-    if nargin>3&&normalize
-    output=output.*(sum(abs(input(:)))/sum(abs(output(:))));
+    if is2d
+        output = ift2(Foutput);
+    else
+        output=fftshift(ifft(ifftshift(Foutput)));
     end
+    
 end
-
